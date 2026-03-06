d965b8deaa5cddce96db76acba37abc3da37af7e
sumith-06 <agirisumith06@gmail.com>
2026-03-06 22:41:31 +0530

    feature: add task API with Flask endpoints

--- static/script.js
+++ static/script.js
@@ -1,9 +1,27 @@
-function addTask(){
-  const input=document.getElementById("taskInput")
-  const taskText=input.value
-  if(taskText==="") return
-  const li=document.createElement("li")
-  li.innerText=taskText
-  document.getElementById("taskList").appendChild(li)
-  input.value=""
+async function addTask() {
+const input = document.getElementById("taskInput")
+const taskText = input.value
+if(taskText === "") return
+await fetch("/api/tasks",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({task:taskText})
+})
+loadTasks()
+
+input.value=""
 }
+async function loadTasks(){
+const response = await fetch("/api/tasks")
+const tasks = await response.json()
+const list = document.getElementById("taskList")
+list.innerHTML=""
+tasks.forEach(task => {
+const li=document.createElement("li")
+li.innerText=task
+list.appendChild(li)
+})
+}
+window.onload = loadTasks
